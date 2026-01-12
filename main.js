c9df2f58d885ef8f50239122f805c4937bf0e9b8
@LMendoza70 <luis.mendoza@uthh.edu.mx>
2026-01-12 09:04:46 -0600

    first commit

---
+++ main.js
@@ -0,0 +1,38 @@
+const coleccion_docentes=[
+    {
+        nombre:"Luis Alberto",
+        apellidos:"Mendoza San Juan",
+        puesto:"Profesor investigador",
+        edad:41,
+        estado:true
+    },
+    {
+        nombre:"med. Efren",
+        apellidos:"Juarez Castillo",
+        puesto:"Profesor investigador",
+        edad:55,
+        estado:false
+    },
+    {
+        nombre:"Hermes",
+        apellidos:"Salazar Casanova",
+        puesto:"Profesor investigador",
+        edad:43,
+        estado:true
+    }
+]
+
+const mostrar=()=>{
+    //rescatamos el div que va a contener la informacion 
+    const contenedor = document.getElementById("contenedor")
+
+    coleccion_docentes.forEach((docente)=>{
+        if(docente.estado==true){
+            contenedor.innerHTML+="<div class='tarjeta'>"+
+            "<h2>"+docente.nombre+"</h2>"+"</div>"
+        }
+
+    })
+
+    
+}
